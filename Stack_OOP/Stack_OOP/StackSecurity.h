6ff0e594981639c95a4381bd700f72959fde9eb1
Евгений Зорин <zorin.eugene@gmail.com>
2019-11-14 21:19:51 +0600

    Canary system added.

--- Stack_OOP/Stack_OOP/StackSecurity.h
+++ Stack_OOP/Stack_OOP/StackSecurity.h
@@ -13,9 +13,11 @@ StackSecurity::StackSecurity() {
 	srand((unsigned int)std::time(0));
 	canarySample = (char)rand();
 }
+//Returns true if canary is OK
 bool StackSecurity::canaryTest(char canary) {
 	return canary == canarySample;
 }
+//Returns canary sample
 char StackSecurity::GetCanarySample() {
 	return canarySample;
 }
